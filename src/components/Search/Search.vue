13ece6b5af2deb342b79e13296d54fbe080cf7f0
Andrii <A.I.Khmelovsky@gmail.com>
2019-08-12 11:05:01 +0300

    Created templates for components

---
+++ src/components/Search/Search.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    v-toolbar
+        h1 HELLO
+        v-text-field(hide-details prepand-icon="search" single-line)
+</template>
+
+<script>
+ export default {
+  name: 'Search',
+  components: {
+    Search,
+  },
+};
+</script>
+<style lang="stylus" scoped>
+  @import '~Styles/_variables'
+</style>
--- src/pages/Main/Main.vue
+++ src/pages/Main/Main.vue
@@ -1,11 +1,21 @@
 <template lang="pug">
   .PageMain
+    Search
+
 
 </template>
 
 <script>
+import Search from 'Components/Search/Search';
 
+ export default {
+  name: 'Main',
+  components: {
+    Search,
+  },
+};
 </script>
+
 
 <style lang="stylus" scoped>
 
