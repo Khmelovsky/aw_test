f64ad6e7e7e228f1211f9d7c8db475657dd377a6
Andrii <A.I.Khmelovsky@gmail.com>
2019-08-14 09:30:34 +0300

    Add New modal structure

--- src/components/AddNew/AddNew.vue
+++ src/components/AddNew/AddNew.vue
@@ -4,19 +4,28 @@
         v-icon(x-large) add
     v-dialog(v-model="dialog" max-width="500")
       v-card
+        v-card-title.headline.primary.lighten-0(primary-title)
+          | Add New
+
         v-tabs(v-model="tab")
           v-tab
-            | Cources
+            | Cource
           v-tab
-            | Users
+            | User
 
         v-tabs-items(v-model="tab")
-          v-tab-item
-            v-card-text
-              | AAAAAAAAAAAA
-          v-tab-item
-            v-card-text
-              | QQQQQQQQQQQQ
+          v-tab-item.modalForm
+            v-form
+              v-text-field(label="Name" required)
+              v-text-field(label="Code" required)
+              v-btn(block color="primary" dark) Submit
+
+          v-tab-item.modalForm
+            v-form
+              v-text-field(label="Name" required)
+              v-text-field(label="E-mail" required)
+              v-text-field(label="Status" required)
+              v-btn(block color="primary" dark) Submit
 </template>
 
 <script>
@@ -26,16 +35,24 @@ export default {
     return {
       dialog: false,
       tab: null,
-      items: [
-        'Appetizers', 'Entrees', 'Deserts', 'Cocktails',
-      ],
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
     }
   },
 };
 </script>
 
 <style lang="stylus" scoped>
-
-
+@import '~Styles/_variables'
+  .v-tabs
+    &__div
+      width: 50%
+  .v-card
+    &__title
+      color: $white
+  .v-window
+    padding: 30px 20px
+  .modalForm
+    .v-btn
+      text-transform uppercase
+      font-size: 18px
+      letter-spacing 0.5px
 </style>
